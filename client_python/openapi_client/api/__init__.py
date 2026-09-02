40aef1ae34c508640608705698a9fee521607a83
PreMan Autofix <autofix@preman.live>
2026-09-02 05:50:18 +0000

    SDK 0.1.1

---
+++ client_python/openapi_client/api/__init__.py
@@ -0,0 +1,21 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.authentication_api import AuthenticationApi
+from openapi_client.api.default_api import DefaultApi
+from openapi_client.api.mock_api import MockApi
+from openapi_client.api.mock_customers_api import MockCustomersApi
+from openapi_client.api.mock_docs_api import MockDocsApi
+from openapi_client.api.mock_openapi_json_api import MockOpenapiJsonApi
+from openapi_client.api.mock_orders_api import MockOrdersApi
+from openapi_client.api.mock_products_api import MockProductsApi
+from openapi_client.api.mock_reviews_api import MockReviewsApi
+from openapi_client.api.mock_schemas_json_api import MockSchemasJsonApi
+from openapi_client.api.mock_tickets_api import MockTicketsApi
+from openapi_client.api.preman_probe_api import PremanProbeApi
+from openapi_client.api.projects_api import ProjectsApi
+from openapi_client.api.ready_api import ReadyApi
+from openapi_client.api.system_api import SystemApi
+from openapi_client.api.tasks_api import TasksApi
+from openapi_client.api.test_ui_api import TestUiApi
+
